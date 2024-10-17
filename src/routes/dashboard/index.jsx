53ad84bef898ca57c576656e0eff9609b9e3a221
Ikramovdev <mubinullakh@gmail.com>
2024-10-17 09:34:54 +0500

    add Suspence and Lazy

--- src/routes/dashboard/index.jsx
+++ src/routes/dashboard/index.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import {AgentMore, Agents, AgentsAdd, BitcoinEthereum, Cards, Owerview, Payments, Statistics, Transactions, Users} from "../../pages/dashboard"
+import {AgentMore, AgentsAdd, BitcoinEthereum, Cards, Owerview, Payments, Statistics, Transactions, Users} from "../../pages/dashboard"
 import Header from '../../components/Header'
 import Navbar from '../../components/Navbar'
+import Loading from "../../assets/images/loading.png"
+
+const Agents = lazy(() => new Promise((resolve) => {
+  return setTimeout(() => resolve(import("../../pages/dashboard/Agents")),1000)
+}))
 
 const DashboardRoutes = () => {
   return (
@@ -10,11 +15,15 @@ const DashboardRoutes = () => {
           <Header/>
           <div className='flex items-center justify-between'>
               <Navbar/>
-              <div className='w-[80%] h-[100vh] overflow-y-auto'>
+              <div className='w-[80%] relative h-[100vh] overflow-y-auto'>
                 <Routes>
                           <Route path='/' element={<Owerview/>}/>
                           <Route path='/users' element={<Users/>}/>
-                          <Route path='/agents' element={<Agents/>}/>
+                          <Route path='/agents' element={
+                            <Suspense fallback={
+                              <img className='absolute top-0 right-0 bottom-0 left-0 mt-5' src={Loading} alt='Loading...' width={400}/>
+                            }><Agents/></Suspense>
+                          }/>
                           <Route path='/agents/:id' element={<AgentMore/>}/>
                           <Route path='/agents/add' element={<AgentsAdd/>}/>
                           <Route path='/agents/:id/update' element={<AgentsAdd/>}/>
